1bf3a7bfc865e8e65eb9f84f5c52f10e1993e55c
Alykhan Kanji <github@alykhan.com>
2014-08-27 22:06:24 -0400

    Add docstrings and reindent 004

--- 004.py
+++ 004.py
@@ -1,12 +1,14 @@
+"""Find the largest palindrome made from the product of two 3-digit numbers."""
 def is_palindrome(n):
-	for i, c in enumerate(n[:len(n)/2]):
-		if c != n[(len(n)-1) - i]:
-			return False
-	return True
+    """Determine if n is a palindrome"""
+    for i, c in enumerate(n[:len(n)/2]):
+        if c != n[(len(n)-1) - i]:
+            return False
+    return True
 
 max = 0
 for a in range(999, 100, -1):
-	for b in range(999, 100, -1):
-		if (a * b > max) and is_palindrome(str(a * b)):
-			max = a * b
+    for b in range(999, 100, -1):
+        if (a * b > max) and is_palindrome(str(a * b)):
+            max = a * b
 print max
