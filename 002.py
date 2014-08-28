8414c2b2c4eeb45b5d2d16a5a3c961c446ff221e
Alykhan Kanji <github@alykhan.com>
2014-08-27 21:59:02 -0400

    Add docstring and reindent 002

--- 002.py
+++ 002.py
@@ -1,10 +1,11 @@
+"""Find the sum of the even-valued terms in the Fibonacci sequence (<4M)"""
 sum = 0
 a = 1
 b = 1
 while a + b < 4000000:
-	if (a + b) % 2 == 0:
-		sum += a + b
-	temp = a + b
-	a = b
-	b = temp
+    if (a + b) % 2 == 0:
+        sum += a + b
+    temp = a + b
+    a = b
+    b = temp
 print sum
