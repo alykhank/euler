d081a260fec9852003c60fc1ed03a5d55a9ff2af
Alykhan Kanji <github@alykhan.com>
2014-08-27 22:04:34 -0400

    Refactor 003 for whitespace and style

--- 003.py
+++ 003.py
@@ -1,21 +1,23 @@
+"""Find the largest prime factor of the number 600851475143"""
 from math import floor, sqrt
 
-NUM=600851475143
-INT_SQRT_NUM=int(floor(sqrt(NUM)))
+NUM = 600851475143
+INT_SQRT_NUM = int(floor(sqrt(NUM)))
 
 def is_prime(n):
-	if n <= 3:
-		if n <= 1:
-			return False
-		return True
-	if n % 2 == 0 or n % 3 == 0:
-		return False
-	for i in xrange(5, int(floor(sqrt(n))) + 1, 6):
-		if n % i == 0 or n % (i + 2) == 0:
-			return False
-	return True
+    """Determine if n is prime"""
+    if n <= 3:
+        if n <= 1:
+            return False
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for j in xrange(5, int(floor(sqrt(n))) + 1, 6):
+        if n % j == 0 or n % (j + 2) == 0:
+            return False
+    return True
 
 for i in xrange(INT_SQRT_NUM, 1, -1):
-	if NUM % i == 0 and is_prime(i):
-		print i
-		break
+    if NUM % i == 0 and is_prime(i):
+        print i
+        break
