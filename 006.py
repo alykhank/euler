ff5d25c67f72459daecb49b1bffe003571bb9904
Alykhan Kanji <github@alykhan.com>
2015-03-03 01:18:58 -0800

    Solve problem 6

---
+++ 006.py
@@ -0,0 +1,17 @@
+"""Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."""
+
+def sum_of_squares(n):
+    """Calculate sum of squares of first n natural numbers."""
+    sum = 0
+    for i in xrange(n + 1):
+        sum += i ** 2
+    return sum
+
+def square_of_sum(n):
+    """Calculate square of sum of first n natural numbers."""
+    sum = 0
+    for i in xrange(n + 1):
+        sum += i
+    return sum ** 2
+
+print square_of_sum(100) - sum_of_squares(100)
